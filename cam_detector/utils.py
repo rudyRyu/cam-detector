5011f851d0167cf6283e7bc7a45b73df7d219f3d
Rudy <rudy.dev.kor@gmail.com>
2020-04-24 12:55:02 +0900

    Update utils.py
     - Add argparser
    
     - Add logger
    
     - Add refine_features func

--- cam_detector/utils.py
+++ cam_detector/utils.py
@@ -1,54 +1,70 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.stats import norm, ks_2samp
+import argparse
+import json
+import logging
 
-def get_std(data):
-    return np.std([packet[field] for packet in data])
+def logger(*, name='',
+              level=logging.INFO,
+              formatter=''):
+    if name:
+        log = logging.getLogger(name)
+    else:
+        log = logging.getLogger(__name__)
 
-def get_avg(data):
-    return np.average([packet[field] for packet in data])
+    handler = logging.StreamHandler()
+    # handler.setFormatter(logging.Formatter(
+    #                         '%(asctime)-15s [%(levelname)s] %(message)s',
+    #                         '%Y-%m-%d %H:%M:%S'))
 
-def get_cdf1(data):
-    hist, bin_edges = np.histogram(data, bins=len(data), density=True)
-    cdf = np.cumsum(hist*np.diff(bin_edges))
-    return cdf
+    if formatter:
+        handler.setFormatter(logging.Formatter(formatter))
+    else:
+        handler.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
 
-def get_cdf2(data):
-    sorted_data = np.sort(data)
-    cdf = np.arange(len(sorted_data))/float(len(sorted_data)-1)
-    return cdf
+    log.addHandler(handler)
+    log.setLevel(level)
 
-def get_cdf3(data):
-    data_size=len(data)
-    # Set bins edges
-    data_set=sorted(set(data))
-    bins=np.append(data_set, data_set[-1]+1)
+    return log
 
-    # Use the histogram function to bin the data
-    counts, bin_edges = np.histogram(data, bins=bins, density=False)
+log = logger()
 
-    counts=counts.astype(float)/data_size
-
-    # Find the cdf
-    cdf = np.cumsum(counts)
-    return cdf
-
-def get_pld_stability(length_list, split_num=50):
-    block_num = (len(length_list)+(split_num//2)) // split_num
-    if block_num <= 1:
-        return None
-
-    cum_s = 0
-    l1 = length_list[:split_num]
-    for j in range(1, block_num):
-        lj = length_list[j*split_num:(j+1)*split_num]
-        cum_s += ks_2samp(l1, lj).statistic
-
-    stb = cum_s/(block_num-1)
-    return stb
+def parse_config():
+    argparser = argparse.ArgumentParser(description='config')
+    argparser.add_argument(
+        '-c',
+        '--conf',
+        required=True,
+        help='path to a configuration file')
 
 
+    args = argparser.parse_args()
+    config_path = args.conf
 
-if __name__ == '__main__':
-    print(ecdf([3, 55, 0.5, 1.5]))
-    print(ECDF([3, 55, 0.5, 1.5]))
+    with open(config_path) as config_buffer:
+        conf = json.loads(config_buffer.read())
+
+    return conf
+
+
+def refine_features(features):
+
+    all_features = ["length_avg",
+                    "pld_stat_stb",
+                    "pld_pval_stb",
+                    "pld_stat_stb_with_cdf",
+                    "pld_pval_stb_with_cdf",
+                    "duration_std",
+                    "duration_avg",
+                    "bandwidth_std",
+                    "bandwidth_avg",
+                    "flow_bandwidth"]
+
+    refined_features = list(set(all_features) & set(features))
+
+    if len(features) != len(refined_features):
+        log.info("Feature list has been refined.")
+        log.info("----------------------------")
+        log.info("Refined feature list")
+        for feature in refined_features:
+            log.info(f" - {feature}")
+        log.info("----------------------------")
+
